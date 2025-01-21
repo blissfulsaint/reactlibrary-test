ae5fc7879d30b149f9a54c92433835fccb8216ce
Brandon Lisonbee <blissfulsaint99@gmail.com>
2025-01-21 22:19:08 +0000

    Refine storybook stories and prepare components for distribution

--- src/components/LayoutBand/LayoutBand.stories.tsx
+++ src/components/LayoutBand/LayoutBand.stories.tsx
@@ -2,25 +2,37 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 import LayoutBand from './LayoutBand';
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: 'Example/LayoutBand',
   component: LayoutBand,
-  parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-    layout: 'centered',
-  },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  argTypes: {
-  },
 } satisfies Meta<typeof LayoutBand>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Large: Story = {
-    args: {
-    }
+export const Default: Story = {
+  args: {
+    children: (
+      <div>
+        <h1 className='block bg-slate-800 p-2 rounded'>This is inside a default LayoutBand!</h1>
+        <p className='block bg-slate-800 p-2 rounded'>
+          The `LayoutBand` component centers its children and applies a max width
+          with padding. Resize the screen to see how it behaves responsively.
+        </p>
+      </div>
+    )
+  }
+};
+
+export const WithMultipleChildren: Story = {
+  args: {
+    children: (
+      <>
+        <div className='bg-emerald-700 p-3'>Child 1</div>
+        <div className='bg-indigo-700 p-3'>Child 2</div>
+        <div className='bg-rose-700 p-3'>Child 3</div>
+      </>
+    ),
+  },
 };
