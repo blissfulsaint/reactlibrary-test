68d6896927a9907c7fd41be97c15b77282e58887
Brandon Lisonbee <blisonbee213596@gmail.com>
2025-01-21 19:34:36 -0700

    Update peer dependencies to include react, react-dom, next to avoid conflicts in React apps

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,16 +4,28 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/), and this project adheres to [Semantic Versioning](https://semver.org/).
 
-## [1.0.2] - 2025-01-21
+## [1.0.3] - 2025-01-21
+
 ### Fixed
+
+- Update package.json to include react, react-dom, next as peerDependencies rather than dependencies to avoid conflicts
+
+## [1.0.2] - 2025-01-21
+
+### Fixed
+
 - Change types in package.json to point to dist/src/index.d.ts for declarations
 
 ## [1.0.1] - 2025-01-21
+
 ### Fixed
+
 - Bug that caused components to not be declared properly in package build
 
 ## [1.0.0] - 2025-01-21
+
 ### Added
+
 - Initial release with basic Storybook setup.
 - Tailwind CSS integration.
 - LayoutBand and Separator components added.
--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,29 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
 export default {
-  input: 'src/index.ts', // Entry file
+  input: "src/index.ts",
   output: [
     {
-      file: 'dist/index.js',
-      format: 'cjs',
+      file: "dist/index.js",
+      format: "cjs",
       sourcemap: true,
     },
     {
-      file: 'dist/index.esm.js',
-      format: 'esm',
+      file: "dist/index.esm.js",
+      format: "esm",
       sourcemap: true,
     },
   ],
   plugins: [
-    peerDepsExternal(), // Exclude peer dependencies like react
-    resolve(),          // Resolve node_modules
-    commonjs(),         // Convert CommonJS to ESModules
-    typescript({        // Handle TypeScript files
-      tsconfig: './tsconfig.json',
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript({
+      tsconfig: "./tsconfig.json",
     }),
   ],
-  external: ['react', 'react-dom'], // Mark these as external
+  external: ["react", "react-dom", "next"],
 };
